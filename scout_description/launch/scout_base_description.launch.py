162d81495f7b6c95a0c518d28ee6373f9157ecbc
KSU-Chad <cnb4966@ksu.edu>
2025-11-30 02:26:54 -0600

    working in rviz2 and gazebo

--- scout_description/launch/scout_base_description.launch.py
+++ scout_description/launch/scout_base_description.launch.py
@@ -4,7 +4,7 @@ import launch_ros
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, AppendEnvironmentVariable
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import FindExecutable, PathJoinSubstitution
 from launch.substitutions import LaunchConfiguration, Command
@@ -12,7 +12,11 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    model_name = 'scout_v2.xacro'
+
+    scout_share = get_package_share_directory('scout_description')
+    model_root = os.path.dirname(scout_share)
+    
+    model_name = 'scout_v2/scout_v2.xacro'
     # model_path = os.path.join(get_package_share_directory('scout_description'), "urdf", model_name)
     # print(model_path)
     robot_description_content = Command([
@@ -23,11 +27,22 @@ def generate_launch_description():
     ])
 
     return launch.LaunchDescription([
-        DeclareLaunchArgument('use_sim_time', default_value='false',
+        DeclareLaunchArgument('use_sim_time', default_value='true',
             description='Use simulation clock if true'),
 
         launch.actions.LogInfo(msg='use_sim_time: '),
         launch.actions.LogInfo(msg=launch.substitutions.LaunchConfiguration('use_sim_time')),
+        
+        #---test 
+        # --- Make Gazebo able to resolve model://scout_description/... ---
+        AppendEnvironmentVariable(
+            name='GZ_SIM_RESOURCE_PATH',
+            value=model_root + os.pathsep
+        ),
+        AppendEnvironmentVariable(
+            name='IGN_GAZEBO_RESOURCE_PATH',
+            value=model_root + os.pathsep
+        ),
         
         launch_ros.actions.Node(
             package='robot_state_publisher',
